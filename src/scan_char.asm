54e707619e8e06846b4fcb7e9f1b3c963289d407
edmarsanchez0618 <edmar.sanchez@ingenieria.unam.edu>
2026-03-13 13:07:10 -0600

    Update scan_char.asm

--- src/scan_char.asm
+++ src/scan_char.asm
@@ -4,22 +4,35 @@ char_buffer resb 1
 SECTION .text
 global scan_char
 
-; ---------------------------------
-; scan_char
-; Salida:
-;   AL = caracter leído
-; ---------------------------------
-
 scan_char:
-
+    ; Prologo de funcion
     push ebp
     mov ebp, esp
 
-    ; TODO:
-    ; 1. usar syscall read
-    ; 2. leer 1 byte desde stdin
-    ; 3. devolverlo en AL
+    push ebx
+    push ecx
+    push edx
+    push eax    ; Guardamos EAX completo
 
+    ; Llamos al sistema read
+    mov eax, 3
+    mov ebx,0
+    mov ecx, char_buffer
+    mov edx, 1
+    int 0x80    ;Llamada al sistema
+
+    ; Restaurando EAX original que fue modificado por sys_read
+    pop eax
+
+    ; Salida: Carga el caracter leido exclusivamente en AL
+    mov al, byte [char_buffer]
+
+    ; Restauramos los registros
+    pop edx
+    pop ecx
+    pop ebx
+
+    ; Epilogo de funcion
     mov esp, ebp
     pop ebp
     ret
