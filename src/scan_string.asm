d7f35517f47ec0fd1d7d4c07b94cc7d46a2cdb87
edmarsanchez0618 <edmar.sanchez@ingenieria.unam.edu>
2026-03-13 13:34:03 -0600

    Update scan_string.asm

--- src/scan_string.asm
+++ src/scan_string.asm
@@ -1,17 +1,32 @@
+; ==============================================================================
+; Función: scan_string
+; Descripción: Lee una cadena desde la entrada estándar (stdin).
+; Entrada: 
+;   EAX = dirección del buffer destino.
+;   EBX = tamaño máximo del buffer.
+; Salida: La cadena queda guardada en el buffer y terminada en cero.
+; ==============================================================================
+
+
+
+
+
 SECTION .text
 global scan_string
 
-; ---------------------------------
-; scan_string
-; Entrada:
-;   EAX = buffer destino
-;   EBX = tamaño máximo
-; ---------------------------------
-
 scan_string:
 
+    ; Prólogo de la función requerido
     push ebp
     mov ebp, esp
+
+    ; Se guardan los registros que se van a modificar
+    push eax
+    push ebx
+    push ecx
+    push edx
+    push esi
+    push edi
 
     ; TODO:
     ; 1. syscall read
