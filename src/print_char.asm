854a6bcaace1d3b47d5d1a0003907a16ef8a34e7
edmarsanchez0618 <edmar.sanchez@ingenieria.unam.edu>
2026-03-13 12:56:22 -0600

    Update print_char.asm

--- src/print_char.asm
+++ src/print_char.asm
@@ -1,3 +1,6 @@
+SECTION .bss
+char_buffer resb 1 ; Reserva 1 byte para el caracter
+
 SECTION .text
 global print_char
 
@@ -12,12 +15,27 @@ print_char:
     push ebp
     mov ebp, esp
 
-    ; TODO:
-    ; 1. Guardar el caracter en memoria
-    ; 2. Usar syscall write
-    ; 3. Imprimir 1 byte
+    ; Se guardan los registros que se van a modificar
+    push eax
+    push ebx
+    push ecx
+    push edx
 
-    ; write(fd=1, buffer, 1)
+    mov [char_buffer], al ; Movemos el caracter AL a la variable global en memoria
+
+    ; Configuramos la llamada al sistema sys_write
+    mov eax, 4            ; Numero de syscall para sys_write
+    mov ebx, 1            
+    mov ecx, char_buffer   ; Puntero a la variable global que contiene el caracter
+    mov edx, 1            ; Longitud a imprimir 1 byte
+
+    int 0x80 ; Llamada al sistema
+
+    ; Restauracion de los registros en orden inverso
+    pop edx
+    pop ecx
+    pop ebx
+    pop eax
 
     mov esp, ebp
     pop ebp
